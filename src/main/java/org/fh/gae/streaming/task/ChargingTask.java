7c48e9b3015227d1812b805256595819b7769dfa
wanghongfei <brucewhf@gmail.com>
2017-12-11 16:10:39 +0800

    kafka流

--- src/main/java/org/fh/gae/streaming/task/ChargingTask.java
+++ src/main/java/org/fh/gae/streaming/task/ChargingTask.java
@@ -2,74 +2,119 @@ package org.fh.gae.streaming.task;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
 import scala.Tuple2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChargingTask {
     public void run(String appname) {
         SparkConf conf = new SparkConf().setAppName(appname);
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaStreamingContext ctx = new JavaStreamingContext(conf, Durations.seconds(10));
 
-        // 读日志
-        JavaRDD<String> allLogRdd = sc.textFile("/Users/whf/projects/open/gae-log-streaming/data/all.log");
-        // 日志分类
-        JavaRDD<String> exposeLogRdd = allLogRdd.filter((line) -> line.startsWith("1"));
-        JavaRDD<String> searchLogRdd = allLogRdd.filter((line) -> line.startsWith("0"));
+        // 检索日志流
+        JavaDStream<String> searchStream = createSearchStream(ctx);
+        // JavaDStream<String> searchStream = createKafkaSearchStream(ctx);
+        // 曝光日志流
+        JavaDStream<String> exposeStream = createExposeStream(ctx);
+        // JavaDStream<String> exposeStream = createKafkaExposeStream(ctx);
+
+        ctx.remember(Durations.seconds(20));
+        final JavaPairRDD<String, ExposeLog>[] lastRdd = new JavaPairRDD[1];
+
 
         // 曝光日志pair
-        JavaPairRDD<String, ExposeLog> exposePairRdd = exposeLogRdd.mapToPair((line) -> {
-            String[] terms = line.split("\t");
-            if (terms.length != 4) {
-                return null;
-            }
+        JavaPairDStream<String, ExposeLog> exposePairStream = exposeStream
+                // 单词切分
+                .map( line -> line.split("\t") )
+                // 按长度过虑
+                .filter( terms -> terms.length == 4 )
+                // 转换成pair
+                .mapToPair(terms -> {
+                    String sid = terms[1];
+                    ExposeLog exposeLog = new ExposeLog(Long.parseLong(terms[2]));
+                    return new Tuple2<>(sid, exposeLog);
+                })
+                // 去重
+                .reduceByKey((log1, log2) -> log1)
+                .transformToPair((Function<JavaPairRDD<String, ExposeLog>, JavaPairRDD<String, ExposeLog>>) pairRdd -> {
+                    if (null == lastRdd[0]) {
+                        lastRdd[0] = pairRdd;
+                        return pairRdd;
+                    }
 
-            String sid = terms[1];
-            ExposeLog exposeLog = new ExposeLog(Long.parseLong(terms[2]));
-            return new Tuple2<>(sid, exposeLog);
-        });
+                    pairRdd = pairRdd.subtractByKey(lastRdd[0]);
+                    lastRdd[0] = pairRdd;
+
+                    return pairRdd;
+                });
+        exposePairStream.print();
+
+
 
         // 检索日志pair
-        JavaPairRDD<String, SearchLog> searchPairRdd = searchLogRdd.mapToPair((line) -> {
-            String[] terms = line.split("\t");
-            if (terms.length != 20) {
-                return null;
-            }
+        JavaPairDStream<String, SearchLog> searchPairStream = searchStream
+                // 单词切分
+                .map( line -> line.split("\t") )
+                // 按长度过虑
+                .filter( terms -> terms.length == 20 )
+                // 转换成pair
+                .mapToPair((terms) -> {
+                    String sid = terms[1];
+                    SearchLog searchLog = new SearchLog(sid, Long.parseLong(terms[19]), Long.parseLong(terms[2]));
+                    return new Tuple2<>(sid, searchLog);
+                });
+        searchPairStream.print();
 
-            String sid = terms[1];
-            SearchLog searchLog = new SearchLog(sid, Long.parseLong(terms[19]), Long.parseLong(terms[2]));
-            return new Tuple2<>(sid, searchLog);
-        });
+        // 两日志JOIN拼接
+        JavaPairDStream<String, Tuple2<SearchLog, ExposeLog>> joinedPairStream = searchPairStream.join(exposePairStream);
 
-        // join
-        JavaPairRDD<String, Tuple2<SearchLog, ExposeLog>> joinedPairRdd = searchPairRdd.join(exposePairRdd);
 
         // map
-        JavaPairRDD<String, JoinedLog> joinRdd = joinedPairRdd.mapValues((tuple) -> {
+        JavaPairDStream<String, JoinedLog> joinStream = joinedPairStream.mapValues((tuple) -> {
             SearchLog searchLog = tuple._1;
-            // ExposeLog exposeLog = tuple._2;
 
             return new JoinedLog(searchLog.getSid(), searchLog.getBid(), searchLog.getTs());
         });
 
-        joinRdd.saveAsTextFile("/Users/whf/projects/open/gae-log-streaming/target/joined");
+        processResult(joinStream);
+
+
+        ctx.start();
+        ctx.awaitTermination();
     }
 
-    private long toLong(String str) {
-        try {
-            return Long.parseLong(str);
-
-        } catch (NumberFormatException e) {
-            return -1;
-        }
+    private JavaDStream<String> createExposeStream(JavaStreamingContext ctx) {
+        return ctx.socketTextStream("whf-mbp.local", 8888);
     }
 
-    private int toInt(String str) {
-        try {
-            return Integer.parseInt(str);
+    private JavaDStream<String> createSearchStream(JavaStreamingContext ctx) {
+        return ctx.socketTextStream("whf-mbp.local", 7777).window(Durations.seconds(20), Durations.seconds(10));
+    }
 
-        } catch (NumberFormatException e) {
-            return -1;
-        }
+    private JavaDStream<String> createKafkaSearchStream(JavaStreamingContext ctx) {
+        Map<String, Integer> topicMap = new HashMap<>();
+        topicMap.put("dev-gae-search", 1);
+
+        return KafkaUtils.createStream(ctx, "10.115.238.30:8701", "gae-log-streaming", topicMap)
+                .map( tuple -> tuple._2 );
+    }
+
+    private JavaDStream<String> createKafkaExposeStream(JavaStreamingContext ctx) {
+        Map<String, Integer> topicMap = new HashMap<>();
+        topicMap.put("dev-gae-expose", 1);
+
+        return KafkaUtils.createStream(ctx, "10.115.238.30:8701", "gae-log-streaming", topicMap)
+                .map( tuple -> tuple._2 );
+    }
+
+    private void processResult(JavaPairDStream<String, JoinedLog> joinedStream) {
+        joinedStream.print();
     }
 }
